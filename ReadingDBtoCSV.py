2289de55613b600935d412a8b0706c35e4d4845a
maddylucky <manvendravschauhan2011@gmail.com>
2021-04-27 09:13:02 +0530

    Update ReadingDBtoCSV.py
    Update comment for Oracle DB reference

--- ReadingDBtoCSV.py
+++ ReadingDBtoCSV.py
@@ -1,8 +1,13 @@
 import pyodbc
 import pandas as p
 
+# Connect Query for My Access DB
 conn = pyodbc.connect(
-    r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\manvendra.1.s\Documents\Test.accdb;')
+    r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=<Location_of_MSAcess>\Test.accdb;')
+
+# Connection Query for Oracle
+# import cx_Oracle
+# con = cx_Oracle.connect('username/password@<HOST>')
 
 SQL_query = p.read_sql_query('select Username, City from UserData', conn)
 
